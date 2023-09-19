5748f4e600f35e9e79a96f9211b0c2f4bdcd0f0a
AcarLibby <113949950+AcarLibby@users.noreply.github.com>
2023-09-19 12:26:10 -0400

    Update Streamlit_app.py
    menu

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
 
 streamlit.title('my parents hew healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
 
