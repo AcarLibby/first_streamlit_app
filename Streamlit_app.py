2e491602b5f6871f5d70f18ef21c2a5cb64b5fc9
AcarLibby <113949950+AcarLibby@users.noreply.github.com>
2023-09-19 17:50:44 -0400

    Update Streamlit_app.py
    changed

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -15,5 +15,5 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("pick some fruits:", list(my_fruit_list.index)
              
-streamlit.multiselect("pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberry'])
+
              
