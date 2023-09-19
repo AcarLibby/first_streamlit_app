fddabfa5c8dae431584847fdec8a9433818c5d3a
AcarLibby <113949950+AcarLibby@users.noreply.github.com>
2023-09-19 12:39:40 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('🥣 my parents hew healthy diner')
+streamlit.title('🥣 My parents New Healthy Diner')
 streamlit.header('🥗 Breakfast Menu')
 streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥑Kale, Spinach & Rocket Smoothie')
