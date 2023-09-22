7575352a243b6edc552227c9555e8e4a61d7884c
AcarLibby <113949950+AcarLibby@users.noreply.github.com>
2023-09-22 13:51:50 -0400

    Update Streamlit_app.py
    attempting again to dragon fruit instead of kiwi

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -39,7 +39,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "dragon fruit")
 #streamlit.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
