552dec50cee9aa8b544d5312bb6b75ee8d698357
AcarLibby <113949950+AcarLibby@users.noreply.github.com>
2023-09-28 16:09:18 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -47,7 +47,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
-(my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 
 
 
